433e90379f82c5a22f8cfd06f218b5b43588c6b2
ElizaG00d <gooden.laurel.232@outlook.com>
2023-05-03 18:01:33 -0500

    delete, find, update by id, single id find

--- nodejs-express-mongodb/app/controllers/tutorial.controller.js
+++ nodejs-express-mongodb/app/controllers/tutorial.controller.js
@@ -32,25 +32,104 @@ exports.create = (req, res) => {
 
 //find single tutorial w id
 exports.findOne = (req, res) => {
+    const id = req.params.id;
 
+    Tutorial.findById(id)
+        .then(data => {
+            if (!data)
+                res.status(404).send({ message: "Not found Tutorial with id " + id });
+            else res.send(data);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .send({ message: "Error retrieving Tutorial with id=" + id });
+        });
 };
 
 //update tutorial by id in req
 exports.update = (req, res) => {
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Data to update can not by empty!"
+        });
+    }
 
+    const id = req.params.id;
+
+    Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+                });
+            } else res.send({ message: "Error updating Tutorial with id=" id });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Tutorial with id=" + id
+            });
+        });
 };
 
 //del tutorial w specified id in req
 exports.delete = (req, res) => {
+    const id = req.params.id;
 
+    Tutorial.findIdAndRemove(id)
+        .then(data => {
+            if (!data) {
+                res.status.(404).send({
+                    message: `Cannot find Tutorial with id=${id}. Maybe Tutorial was not found!`
+                });
+            } else {
+                res.send({
+                    message: "Tutorial was deleted successfully!"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Tutorial with id=" + id
+            });
+        });
 };
 
 //del all tutorials from db
 exports.deleteAll = (req, res) => {
-
+    Tutorial.deleteMany({})
+        .then(data => {
+            res.send({
+                message: `${data.deleteCount} Tutorials were deleted successfully!`
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while removing all tutorials."
+            });
+        });
 };
 
 //find all published tutorials
 exports.findAllPublished = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { $regex: new RegExp(title), $options: "i" }} : { };
 
+    //Tutorial.find({ published: true })
+
+    //?controller can be modded for pagination response?
+
+    Tutorial.find(condition)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                err.message || "Some error occurred while retrieving tutorials."
+            });
+        });
 };
+
+//req.query.title to get query string from req and consider it as condition for findAll()

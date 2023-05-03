fb902db898846406273306c33f0496e54963a9d6
ElizaG00d <gooden.laurel.232@outlook.com>
2023-05-03 17:21:45 -0500

    tutorial create save and new

--- nodejs-express-mongodb/app/controllers/tutorial.controller.js
+++ nodejs-express-mongodb/app/controllers/tutorial.controller.js
@@ -3,7 +3,31 @@ const Tutorial = db.tutorials;
 
 //create and save new tutorial
 exports.create = (req, res) => {
+    //validate req
+    if (!req.body.title) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        return;
+    }
 
+    //create tutorial
+    const tutorial = new Tutorial({
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    });
+
+    //save tutorial in the db
+    tutorial
+        .save(tutorial)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                err.message || "Some error occured while creating the Tutorial."
+            });
+        });
 };
 
 //find single tutorial w id

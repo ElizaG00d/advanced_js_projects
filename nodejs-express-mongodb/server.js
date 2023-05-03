06a9456148c0829f6c75ef2f844bfb100050d993
ElizaG00d <gooden.laurel.232@outlook.com>
2023-05-03 15:47:14 -0500

    define and config mongoose

--- nodejs-express-mongodb/server.js
+++ nodejs-express-mongodb/server.js
@@ -9,6 +9,20 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
+const db = require('./app/models');
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to the database!');
+    })
+    .catch(err => {
+        console.log('Cannot connect to the database!', err);
+        process.exit();
+    });
+
 //parse req of content type - application/json
 app.use(express.json());
 
